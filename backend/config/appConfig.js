560523a8f812ddfc7b974486b49709626a51f17c
DanyalAnsari <mh.danyalansari@gmail.com>
2025-07-15 23:32:30 +0530

    debug

--- backend/config/appConfig.js
+++ backend/config/appConfig.js
@@ -11,8 +11,7 @@ dotenv.config({
 	path: path.resolve(__dirname, "../.env"),
 });
 
-
-export const PORT = process.env.PORT || 5000;
+export const PORT = Number(process.env.PORT) || 5000;
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5000";
 export const MONGO_DB_URI = process.env.MONGO_DB_URI;
